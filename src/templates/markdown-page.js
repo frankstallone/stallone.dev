3b45eaea28505dc9d774fefa9433a4f6e8d1a869
Frank Stallone III <Frank.Stallone@adp.com>
2019-06-05 13:07:33 -0400

    wip: 🚧  Getting about page layout to work

--- src/templates/markdown-page.js
+++ src/templates/markdown-page.js
@@ -2,10 +2,25 @@ import React from 'react'
 import Container from 'components/Container'
 import SEO from '../components/SEO'
 import Layout from '../components/Layout'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const MarkdownPage = ({ children, pageContext: { frontmatter } }) => {
+  const data = useStaticQuery(graphql`
+    query LayoutQuery {
+      site {
+        siteMetadata {
+          title
+          description
+          author {
+            name
+          }
+          keywords
+        }
+      }
+    }
+  `)
   return (
-    <Layout noSubscribeForm>
+    <Layout site={data.site} noSubscribeForm>
       <SEO frontmatter={frontmatter} />
       <Container noVerticalPadding>{children}</Container>
     </Layout>
